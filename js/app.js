7fc8eb07f77a15488107627f568529ce447666c7
Sebastian Slomski <me@sebslomski.com>
2012-02-08 23:26:27 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,81 @@
+$(function () {
+  function elementInViewport(el) {
+    var rect = el.getBoundingClientRect()
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= window.innerHeight &&
+      rect.right <= window.innerWidth
+    );
+  }
+
+  document.addEventListener('scroll', function () {
+    $('h1').each(function (elem) {
+      if (elementInViewport(this)) {
+        var id = $(this).parents('section').attr('id');
+        if (id) {
+          $('.bar-menu a[href="#' + id + '"]').trigger('click');
+        }
+      }
+    });
+  });
+
+  $(document).trigger('scroll');
+
+  if (!$('html.cssanimations.csstransitions').length) {
+    $('#browser-fail').show();
+  }
+
+  $('a.alert-close').click(function (e) {
+    e.preventDefault();
+    $(e.target).parents('.alert-box-wrapper').animate({height: 0}, 1000);
+  });
+
+  $('.bar-logo a').click(function (e) {
+    e.preventDefault();
+    $('.bar-menu li a.s-active').removeClass('s-active');
+    $(window).scrollTo(0, 1000);
+  });
+
+  $('.bar-menu li a').click(function (e) {
+    e.preventDefault();
+    $(this).parents('li').siblings().find('a.s-active').removeClass('s-active');
+    $(this).addClass('s-active');
+    if (!e.isTrigger) {
+      var $fixedBar = $('.bar-fixed');
+      var $targetElem = $($(this).attr('href'));
+      $(window).scrollTo($targetElem.position().top, 1000);
+    }
+  });
+
+
+  $('.portfolio-detail-close').click(function (e) {
+    e.preventDefault();
+    if ($('html.cssanimations').length) {
+      $('.portfolio-detail-container')
+        .removeClass('fadeInUp')
+        .addClass('fadeOutUp');
+    } else {
+      $('.portfolio-detail-container').animate({height: 0}, 1000);
+    }
+  });
+
+  $('.portfolio-preview img').click(function (e) {
+    if ($('html.cssanimations').length) {
+      $('.portfolio-detail-container')
+        .removeClass('fadeOutUp')
+        .addClass('fadeInUp');
+    } else {
+      $('.portfolio-detail-container').animate({height: 400}, 1000);
+    }
+
+    $('.portfolio-slides').cycle({
+      fx: 'scrollHorz',
+      prev: '.portfolio-control-left',
+      next: '.portfolio-control-right',
+      timeout: 0
+    });
+
+    $('.bar-menu li a[href="#portfolio"]').trigger('click');
+  });
+});
